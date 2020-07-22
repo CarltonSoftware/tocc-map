1262701fb402c4017ea150b27563e08e3a40e86c
Alex <alex.wyett@gmail.com>
2020-07-22 18:02:41 +0100

    Updates

---
+++ precache-manifest.dd167ab1ef5ec6146d3c4aa1ba216430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe9d1a8c0ef49128b6de8afc4fae216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1410429ed2de73f126f",
+    "url": "/static/css/main.6c6f4177.chunk.css"
+  },
+  {
+    "revision": "ec07b50ee29195c7ad0b",
+    "url": "/static/js/2.16a7ef54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16a7ef54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1410429ed2de73f126f",
+    "url": "/static/js/main.c7d03fb7.chunk.js"
+  },
+  {
+    "revision": "d5d46e225ba877be1e32",
+    "url": "/static/js/runtime-main.6c3f79bc.js"
+  }
+]);
